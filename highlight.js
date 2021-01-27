a5c9244ddf3b08de3fbd691b3ca3a9255d3512a9
kmaasrud <km@aasrud.com>
2021-01-27 21:57:06 +0100

    Perhaps if everything is HTML?

--- highlight.js
+++ highlight.js
@@ -1,4 +1,3 @@
 document.write('<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/styles/default.min.css">\
-<script src="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/highlight.min.js"></script>');
-hljs.configure({noHighlightRe: /^mermaid$/});
-hljs.initHighlightingOnLoad();
+<script src="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/highlight.min.js"></script>\
+<script>hljs.configure({noHighlightRe: /^mermaid$/}); hljs.initHighlightingOnLoad();</script>');
