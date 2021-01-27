159874ca0c99d5f1449f092c4baa608f839fb3fd
kmaasrud <km@aasrud.com>
2021-01-27 09:07:43 +0100

    Forgot to change the JS file

--- header.js
+++ header.js
@@ -1,5 +1,4 @@
 document.write('\
-<div id="header">page/<strong>subpage</strong></div>\
 <button id="night-mode" onclick="darkModeToggle()"><div>☽</div></button>\
 <button id="search"><div>⚲</div></button> \
 ');
