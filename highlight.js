dad99b54c01a12107693d14193624fd4562b0105
kmaasrud <km@aasrud.com>
2021-01-27 22:12:14 +0100

    My god I'm stupid

--- highlight.js
+++ highlight.js
@@ -1,3 +1,3 @@
-document.write('<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/styles/default.min.css">\
-<script src="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/monokai.min.js"></script>\
+document.write('<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/styles/monokai.min.css">\
+<script src="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/highlight.min.js"></script>\
 <script>hljs.configure({noHighlightRe: /^mermaid$/}); hljs.initHighlightingOnLoad();</script>');
