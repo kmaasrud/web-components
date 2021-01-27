d96346c489f8f7cd499f061e2bb02c59021b4cbb
kmaasrud <km@aasrud.com>
2021-01-27 21:49:14 +0100

    Added highlight.js as component

---
+++ highlight.js
@@ -0,0 +1,4 @@
+document.write('<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/styles/default.min.css">\
+<script src="https://unpkg.com/@highlightjs/cdn-assets@10.4.0/highlight.min.js"></script>');
+hljs.configure({noHighlightRe: /^mermaid$/});
+hljs.initHighlightingOnLoad();
