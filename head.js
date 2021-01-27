b3299602926a36c5b849e275e45b99965d68c457
kmaasrud <km@aasrud.com>
2021-01-27 08:55:19 +0100

    Added KaTeX script

--- head.js
+++ head.js
@@ -1,5 +1,4 @@
 document.write('\
-<meta charset="UTF-8">\
 <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-<link rel="stylesheet" type="text/css" href="main.css">\
+<link rel="stylesheet" type="text/css" href="https://www.kmaasrud.com/web-components/main.css">\
 ');
